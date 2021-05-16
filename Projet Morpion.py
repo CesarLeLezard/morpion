d3c3f2f96629f97ee3b147dc462c63a5b413665a
Thomas <thomas.guillemet@edu.devinci.fr>
2021-05-16 11:47:43 +0200

    modify utility fonction and 'partie_terminee' function

--- Projet Morpion.py
+++ Projet Morpion.py
@@ -7,6 +7,10 @@ Created on Wed May  5 10:33:29 2021
 import numpy as np
 import time
 
+compteur = 0
+jeton = None
+
+
 class Morpion:
     def __init__(self, x=12, y=12, grille=None):
         if (grille is None) or grille.shape != (x, y):
@@ -38,7 +42,7 @@ def cases_jouables(grille):
     return cases
 
 
-def applique_coup(grille, coup,modifier = False):
+def applique_coup(grille, coup, modifier=False):
     if modifier:
         grille[coup[0]][coup[1]] = coup[2]
     copie_grille = np.copy(grille)
@@ -46,229 +50,180 @@ def applique_coup(grille, coup,modifier = False):
     return copie_grille
 
 
-# vérifie si la partie est terminée
-def partie_terminee(s):
-    flag = True
-    for i in range(s.shape[0]):
-        for j in range(s.shape[1]):
-            if s[i][j] is None:
-                flag = False
-    if not flag:
-        for j in range(s.shape[1]):
-            # colonnes
-            for i in range(s.shape[0] - 3):
-                flag_temp = True
-                if ((s[i][j] != s[i + 1][j]) or (s[i + 1][j] != s[i + 2][j]) or (s[i + 2][j] != s[i + 3][j])) or (
-                        s[i][j] is None):
-                    flag_temp = False
-                flag = flag or flag_temp
-        if not flag:
-            for i in range(s.shape[0]):
-                # lignes
-                for j in range(s.shape[1] - 3):
-                    flag_temp = True
-                    if ((s[i][j] != s[i][j + 1]) or (s[i][j + 1] != s[i][j + 2]) or (s[i][j + 2] != s[i][j + 3])) or (
-                            s[i][j] is None):
-                        flag_temp = False
-                    flag = flag or flag_temp
-            if not flag:
-                for ligne in range(s.shape[0] - 3):
-                    # print("ligne",ligne)
-                    for i in range(min(s.shape[0] - 3 - ligne, s.shape[1] - 3 - ligne)):
-                        # print(i)
-                        flag_temp = True
-                        if ((s[ligne + i][i] != s[ligne + i + 1][i + 1]) or (
-                                s[ligne + i + 1][i + 1] != s[ligne + i + 2][i + 2]) or (
-                                    s[ligne + i + 2][i + 2] != s[ligne + i + 3][i + 3])) or (s[ligne + i][i] == None):
-                            flag_temp = False
-                        flag = flag or flag_temp
-                if not flag:
-                    stemp = np.rot90(s)
-                    for ligne in range(stemp.shape[0] - 3):
-                        for i in range(min(stemp.shape[0] - 3 - ligne, stemp.shape[1] - 3 - ligne)):
-                            flag_temp = True
-                            if ((stemp[ligne + i][i] != stemp[ligne + i + 1][i + 1]) or (
-                                    stemp[ligne + i + 1][i + 1] != stemp[ligne + i + 2][i + 2]) or (
-                                        stemp[ligne + i + 2][i + 2] != stemp[ligne + i + 3][i + 3])) or (
-                                    stemp[ligne + i][i] is None):
-                                flag_temp = False
-                            flag = flag or flag_temp
-    # print(flag)
-    return flag
+def grille_remplie(grille):
+    for i in range(grille.shape[0]):
+        for j in range(grille.shape[1]):
+            if grille[i][j] is None:
+                return False
+    return True
 
 
-def voisins(grille, x, y):
+def test_colonne(grille):
+    global jeton
+    for j in range(grille.shape[1]):
+        for i in range(grille.shape[0] - 3):
+            if grille[i][j] is not None and grille[i][j] == grille[i + 1][j] and grille[i][j] == grille[i + 2][j] \
+                    and grille[i][j] == grille[i + 3][j]:
+                jeton = grille[i][j]
+                return True
+    return False
+
+
+def test_ligne(grille):
+    global jeton
+    for i in range(grille.shape[0]):
+        for j in range(grille.shape[1] - 3):
+            if grille[i][j] is not None and grille[i][j] == grille[i][j + 1] and grille[i][j] == grille[i][j + 2] \
+                    and grille[i][j] == grille[i][j + 3]:
+                jeton = grille[i][j]
+                return True
+    return False
+
+
+def test_diagonale(grille):
+    global jeton
+    for i in range(grille.shape[0] - 3):
+        for j in range(grille.shape[1] - 3):
+            if grille[i][j] is not None and grille[i][j] == grille[i + 1][j + 1] \
+                    and grille[i][j] == grille[i + 2][j + 2] and grille[i][j] == grille[i + 3][j + 3]:
+                jeton = grille[i][j]
+                return True
+    return False
+
+
+def partie_terminee(grille):
+    return grille_remplie(grille) or test_ligne(grille) or test_colonne(grille) or test_diagonale(grille) \
+           or test_diagonale(np.rot90(grille))
+
+
+def voisins_identiques(grille, x, y):
     res = 0
     val = grille[x][y]
     if x + 1 < taille:
-        if grille[x + 1][y] == val:
+        if grille[x + 1][y] == val or grille[x + 1][y] is None:
             res += 1
     if y + 1 < taille:
-        if grille[x][y + 1] == val:
+        if grille[x][y + 1] == val or grille[x][y + 1] is None:
             res += 1
     if x + 1 < taille and y + 1 < taille:
-        if grille[x + 1][y + 1] == val:
+        if grille[x + 1][y + 1] == val or grille[x + 1][y + 1] is None:
             res += 1
     if x - 1 >= 0:
-        if grille[x - 1][y] == val:
+        if grille[x - 1][y] == val or grille[x - 1][y] is None:
             res += 1
     if y - 1 >= 0:
-        if grille[x][y - 1] == val:
+        if grille[x][y - 1] == val or grille[x][y - 1] is None:
             res += 1
     if x - 1 >= 0 and y - 1 >= 0:
-        if grille[x - 1][y - 1] == val:
+        if grille[x - 1][y - 1] == val or grille[x - 1][y - 1] is None:
             res += 1
     if x - 1 >= 0 and y + 1 < taille:
-        if grille[x - 1][y + 1] == val:
+        if grille[x - 1][y + 1] == val or grille[x - 1][y + 1] is None:
             res += 1
     if y - 1 >= 0 and x + 1 < taille:
-        if grille[x + 1][y - 1] == val:
+        if grille[x + 1][y - 1] == val or grille[x + 1][y - 1] is None:
             res += 1
     return res
 
-def absence_voisins(grille, x, y):
+
+def nombre_voisins(grille, x, y):
     res = 0
     if x + 1 < taille:
-        if grille[x + 1][y] == symbole_joueur or grille[x + 1][y]==symbole_ordinateur:
+        if grille[x + 1][y] == symbole_joueur or grille[x + 1][y] == symbole_ordinateur:
             res += 1
     if y + 1 < taille:
-        if grille[x][y + 1] == symbole_joueur or grille[x][y + 1]==symbole_ordinateur:
+        if grille[x][y + 1] == symbole_joueur or grille[x][y + 1] == symbole_ordinateur:
             res += 1
     if x + 1 < taille and y + 1 < taille:
-        if grille[x + 1][y + 1] == symbole_joueur or grille[x + 1][y + 1]==symbole_ordinateur:
+        if grille[x + 1][y + 1] == symbole_joueur or grille[x + 1][y + 1] == symbole_ordinateur:
             res += 1
     if x - 1 >= 0:
-        if grille[x - 1][y] == symbole_joueur or grille[x - 1][y]==symbole_ordinateur:
+        if grille[x - 1][y] == symbole_joueur or grille[x - 1][y] == symbole_ordinateur:
             res += 1
     if y - 1 >= 0:
-        if grille[x][y - 1] == symbole_joueur or grille[x][y - 1]==symbole_ordinateur:
+        if grille[x][y - 1] == symbole_joueur or grille[x][y - 1] == symbole_ordinateur:
             res += 1
     if x - 1 >= 0 and y - 1 >= 0:
-        if grille[x - 1][y - 1] == symbole_joueur or grille[x - 1][y - 1]==symbole_ordinateur:
+        if grille[x - 1][y - 1] == symbole_joueur or grille[x - 1][y - 1] == symbole_ordinateur:
             res += 1
     if x - 1 >= 0 and y + 1 < taille:
-        if grille[x - 1][y + 1] == symbole_joueur or grille[x - 1][y + 1]==symbole_ordinateur:
+        if grille[x - 1][y + 1] == symbole_joueur or grille[x - 1][y + 1] == symbole_ordinateur:
             res += 1
     if y - 1 >= 0 and x + 1 < taille:
-        if grille[x + 1][y - 1] == symbole_joueur or grille[x + 1][y - 1]==symbole_ordinateur:
+        if grille[x + 1][y - 1] == symbole_joueur or grille[x + 1][y - 1] == symbole_ordinateur:
             res += 1
     return res
+
 
 def cases_jouables_interessantes(grille):
     tableau = cases_jouables(grille)
     res = []
     for coup in tableau:
-        if absence_voisins(grille, coup[0],coup[1]) >0:
+        if nombre_voisins(grille, coup[0], coup[1]) > 0:
             res.append(coup)
     return res
 
+
 def evaluation_grille(grille):
-    res = 0
-    flag = False
-    if len(cases_jouables(grille)) == taille * taille:
-        flag = True
-    for j in range(grille.shape[1]):
-        # colonnes
-        for i in range(grille.shape[0] - 3):
-            flagTemp = True
-            if ((grille[i][j] != grille[i + 1][j]) or (grille[i + 1][j] != grille[i + 2][j]) or (grille[i + 2][j] != grille[i + 3][j])) or (
-                    grille[i][j] == None):
-                flagTemp = False
-            if flagTemp:
-                if grille[i][j] == symbole_ordinateur:
-                    res = 1
+    if jeton is not None:
+        return notation[jeton]
+    cases_interessantes = cases_jouables_interessantes(grille)
+    nb_coup_critique = 0
+    for case in cases_interessantes:
+        grille2 = applique_coup(grille, case + [symbole_ordinateur])
+        if test_ligne(grille2) or test_colonne(grille2) or test_diagonale(grille2) or test_diagonale(np.rot90(grille2)):
+            nb_coup_critique += 1
+    if nb_coup_critique > 0:
+        if nb_coup_critique >= 2:
+            return 0.99
+        else:
+            return 0.80
+    for case in cases_interessantes:
+        grille2 = applique_coup(grille, case + [symbole_joueur])
+        if test_ligne(grille2) or test_colonne(grille2) or test_diagonale(grille2) or test_diagonale(np.rot90(grille2)):
+            nb_coup_critique += 1
+    if nb_coup_critique > 0:
+        if nb_coup_critique >= 2:
+            return -0.99
+        else:
+            return -0.80
+    compteur_joueur = 0
+    compteur_ordinateur = 0
+    for i in range(grille.shape[0]):
+        for j in range(grille.shape[1]):
+            if grille[i, j] is not None:
+                voisins_identiques_ou_nuls = voisins_identiques(grille, i, j)
+                if grille[i, j] == symbole_ordinateur:
+                    compteur_ordinateur += voisins_identiques_ou_nuls
                 else:
-                    res = -1
-            flag = flag or flagTemp
-    if not flag:
-        for i in range(grille.shape[0]):
-            # lignes
-            # flagTemp = True
-            for j in range(grille.shape[1] - 3):
-                flagTemp = True
-                if ((grille[i][j] != grille[i][j + 1]) or (grille[i][j + 1] != grille[i][j + 2]) or (grille[i][j + 2] != grille[i][j + 3])) or (
-                        grille[i][j] == None):
-                    flagTemp = False
-                if flagTemp:
-                    if grille[i][j] == symbole_ordinateur:
-                        res = 1
-                    else:
-                        res = -1
-                flag = flag or flagTemp
-        if not flag:
-            for ligne in range(grille.shape[0] - 3):
-                # print("ligne",ligne)
-                for i in range(min(grille.shape[0] - 3 - ligne, grille.shape[1] - 3 - ligne)):
-                    # print(i)
-                    flagTemp = True
-                    if ((grille[ligne + i][i] != grille[ligne + i + 1][i + 1]) or (
-                            grille[ligne + i + 1][i + 1] != grille[ligne + i + 2][i + 2]) or (
-                                grille[ligne + i + 2][i + 2] != grille[ligne + i + 3][i + 3])) or (grille[ligne + i][i] == None):
-                        flagTemp = False
-                    if flagTemp:
-                        if grille[ligne + i][i] == symbole_ordinateur:
-                            res = 1
-                        else:
-                            res = -1
-                    flag = flag or flagTemp
-            if not flag:
-                stemp = np.rot90(grille)
-                for ligne in range(stemp.shape[0] - 3):
-                    for i in range(min(stemp.shape[0] - 3 - ligne, stemp.shape[1] - 3 - ligne)):
-                        flagTemp = True
-                        if ((stemp[ligne + i][i] != stemp[ligne + i + 1][i + 1]) or (
-                                stemp[ligne + i + 1][i + 1] != stemp[ligne + i + 2][i + 2]) or (
-                                    stemp[ligne + i + 2][i + 2] != stemp[ligne + i + 3][i + 3])) or (
-                                stemp[ligne + i][i] == None):
-                            flagTemp = False
-                        if flagTemp:
-                            if stemp[ligne + i][i] == symbole_ordinateur:
-                                res = 1
-                            else:
-                                res = -1
-                        flag = flag or flagTemp
-    if res == 0 and not flag:
-        for i in cases_jouables(grille):
-            if partie_terminee(applique_coup(grille, i + [symbole_ordinateur])):
-                if evaluation_grille(applique_coup(grille, i + [symbole_ordinateur])) == 1:
-                    res = 0.99
-                elif evaluation_grille(applique_coup(grille, i + [symbole_joueur])) == -1:
-                    res = -0.99
-        if res == 0:
-            nbv = 0
-
-            for i in range(taille):
-                for j in range(taille):
-                    # print(i,j)
-                    if grille[i][j] is not None:
-                        nbv += voisins(grille, i, j)
-            res = nbv / ((taille * taille - len(cases_jouables(grille))) * 100)
-    return res
+                    compteur_joueur += voisins_identiques_ou_nuls
+    return (compteur_ordinateur - compteur_joueur) / (grille.shape[0] * grille.shape[1] * 8)
 
 
-def minimax(grille, ordinateur, alpha, beta,profondeur):
-    # print(Morpion(x=12, y=12, grille=grille))
-    if partie_terminee(grille) or profondeur<1:
-        # print(evaluation_grille(grille))
+def minimax(grille, ordinateur, alpha, beta, profondeur):
+    global compteur
+    global jeton
+    jeton = None
+    compteur += 1
+    # print(compteur)
+    if partie_terminee(grille) or profondeur < 1:
         return evaluation_grille(grille)
     elif ordinateur:
         max_evaluation = - np.Inf
         for coup in cases_jouables_interessantes(grille):
-            copie_grille = np.copy(grille)
             coup.append(symbole_ordinateur)
-            evaluation = minimax(applique_coup(copie_grille, coup), False, alpha, beta,profondeur-1)
+            evaluation = minimax(applique_coup(grille, coup), False, alpha, beta, profondeur - 1)
             alpha = max(alpha, evaluation)
-            max_evaluation = max(max_evaluation,evaluation)
+            max_evaluation = max(max_evaluation, evaluation)
             if beta <= alpha:
                 break
         return max_evaluation
     else:
         min_evaluation = np.Inf
         for coup in cases_jouables_interessantes(grille):
-            copie_grille = np.copy(grille)
             coup.append(symbole_joueur)
-            evaluation = minimax(applique_coup(copie_grille, coup), True, alpha, beta,profondeur-1)
-            min_evaluation = min(min_evaluation,evaluation)
+            evaluation = minimax(applique_coup(grille, coup), True, alpha, beta, profondeur - 1)
+            min_evaluation = min(min_evaluation, evaluation)
             beta = min(beta, evaluation)
             if beta <= alpha:
                 break
@@ -282,21 +237,24 @@ def jeu(mp, tour_ordinateur=True):
     while not partie_terminee(mp.grille):
         if tour_ordinateur:
             t = time.time()
-            if len(cases_jouables(mp.grille))== taille*taille:
-                coup = [5,5]
+            coup = []
+            if len(cases_jouables(mp.grille)) == taille * taille:
+                coup = [taille // 2, taille // 2]
             else:
-                copie_grille = np.copy(mp.grille)
-                valeure=-1000
-                for i in cases_jouables_interessantes(copie_grille):
-                    minimax_valeure = minimax(copie_grille, True, - np.Inf, np.Inf,3)
-                    if valeure <= minimax_valeure:
-                        valeure = minimax_valeure
+                valeur = - np.Inf
+                print("Nombres de cases_jouables intéressantes : ", len(cases_jouables_interessantes(mp.grille)))
+                for i in cases_jouables_interessantes(mp.grille):
+                    minimax_valeur = minimax(applique_coup(mp.grille, i + [symbole_ordinateur]), False,
+                                             - np.Inf, np.Inf, profondeur_minimax)
+                    if valeur <= minimax_valeur:
+                        print("minimax : ", minimax_valeur, i)
+                        valeur = minimax_valeur
                         coup = i
             print("\nMon coup (⌐■_■) :", coup, "\n")
-            applique_coup(mp.grille, coup+[symbole_ordinateur],True)
+            applique_coup(mp.grille, coup + [symbole_ordinateur], True)
             print(mp)
             tour_ordinateur = not tour_ordinateur
-            print("Temps de réfléxion: ",time.time()-t)
+            print("Temps de réflexion : ", time.time() - t)
         else:
             print("\nA toi de jouer ヽ(♡‿♡)ノ \n")
             x = int(input("Ta ligne stp (つ✧ω✧)つ :"))
@@ -317,9 +275,8 @@ def jeu(mp, tour_ordinateur=True):
 if __name__ == '__main__':
     symbole_joueur = "O"
     symbole_ordinateur = "X"
+    notation = {symbole_ordinateur: 1, symbole_joueur: -1}
     taille = 12
+    profondeur_minimax = 0
     morpion = Morpion(x=taille, y=taille)
-    # morpion.grille = (np.array([['X','X','O','X',None,None,None,None,None,None,None,None],[None,None,None,None,None,None,None,None,None,None,None,None],[None,None,None,None,None,None,None,None,None,None,None,None],[None,None,None,None,None,None,None,None,None,None,None,None],[None,None,None,None,None,None,None,None,None,None,None,None],[None,None,None,None,None,None,None,None,None,None,None,None],[None,None,None,None,None,None,None,None,None,None,None,None],[None,None,None,None,None,None,None,None,None,None,None,None],[None,None,None,None,None,None,None,None,None,None,None,None],[None,None,None,None,None,None,None,None,None,None,None,None],[None,None,None,None,None,None,None,None,None,None,None,None],[None,None,None,None,None,None,None,None,None,None,None,None]]))
-    # print(len(cases_jouables(morpion.grille)))
-    # print((cases_jouables_interessantes(morpion.grille)))
     jeu(morpion)
